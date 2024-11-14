f628d210baf9e27648cd00b8d7d125008f6de5e2
BelardoA <abelardo@regscale.com>
2024-11-13 23:17:51 -0500

    simplified rename_files, setting new attributes for videofile and better parsing during parse_episode_number_and_name

--- functions.py
+++ functions.py
@@ -5,10 +5,10 @@
 # standard python imports
 import glob
 import os
-import platform
 import re
 import sys
 from pathlib import Path
+from typing import Optional
 
 from rich.console import Console
 
@@ -17,32 +17,32 @@ from video_file import VideoFile
 console = Console()
 
 
-def get_episode_number(episode_name: str) -> int:
+def parse_episode_number_and_name(file_name: str) -> tuple[int, Optional[str]]:
     """
-    Function to parse the provided episode_name for an episode number
+    Function to parse the provided episode_name for an episode number and name
 
-    :param str episode_name: File name for the episode
-    :return: The provided episode_name's episode #
-    :rtype: int
+    :param str file_name: File name for the video file
+    :return: The provided episode_name's episode # and episode name
+    :rtype: tuple[int, Optional[str]]
     """
     # use regex to see if it fits the SXXEXX format
-    if match := re.search(r"S(\d{2,3})E(\d{2,3})", episode_name, re.IGNORECASE):
-        return match.group(2)
+    if match := re.search(r"S(\d{2,3})E(\d{2,3})", file_name, re.IGNORECASE):
+        return int(match.group(2)), file_name.replace(match.group(), "").strip()
     # use regex to find the episode number
-    if match := re.search(r"(\d{2,3})", episode_name):
-        return match.group()
+    if match := re.search(r"(\d{2,3})", file_name):
+        return int(match.group()), file_name.replace(match.group(), "").strip()
     # see if underscores are used
     for split_char in ["_", "-", "."]:
-        if split_char in episode_name:
-            split_episode_name = episode_name.split(split_char)
+        if split_char in file_name:
+            split_episode_name = file_name.split(split_char)
             for item in split_episode_name:
                 try:
                     episode_number = int(item)
-                    return episode_number
+                    return episode_number, None
                 except ValueError:
                     continue
-    console.print(f"[red] Cannot parse episode number from {episode_name}")
-    return 0
+    console.print(f"[red] Cannot parse episode number from {file_name}")
+    return 0, None
 
 
 def get_video_files(file_path: Path) -> list[VideoFile]:
@@ -88,12 +88,17 @@ def get_video_files(file_path: Path) -> list[VideoFile]:
         for folder, files in season_file_mapping.items():
             # parse the needed information from the file path
             for file in files:
+                episode_number, episode_name = parse_episode_number_and_name(file.stem)
                 videos.append(
                     VideoFile(
                         season=int(folder.split(" ")[-1]),
-                        episode=get_episode_number(file.stem),
+                        episode=episode_number,
+                        episode_name=episode_name,
                         file_path=file,
-                        index=files.index(file),
+                        file_name=file.stem,
+                        file_type=file.suffix,
+                        directory=file.parent,
+                        index=files.index(file) + 1,
                     )
                 )
     else:
@@ -105,54 +110,26 @@ def get_video_files(file_path: Path) -> list[VideoFile]:
     return videos
 
 
-def rename_files(file_list: dict) -> int:
+def rename_files(file_list: list[VideoFile]) -> int:
     """
     Function to rename the items in the provided file_list
-    :param dict file_list: dictionary of files that need to be renamed
+    :param list[VideoFile] file_list: List of VideoFiles that need to be renamed
     :return: # of files that were renamed
     :rtype: int
     """
-
-    def format_number(number: str) -> str:
-        """
-        Function to format a number into 2 digit string
-        :param int number: episode or season number to convert
-        :return: XX format of the provided number
-        :rtype: str
-        """
-        return str(number) if len(number) == 2 else f"0{number}"
-
     # create a counter for files renamed
     counter = 0
-
-    for season, episodes in file_list.items():
-        # start episode counter at 1
-        episode_num = 1
-
-        # parse the season #
-        season_num = format_number(season.split(" ")[-1])
-        # iterate through the season's episodes and rename them
-        for episode in episodes:
-            # parse the episode name
-            episode_name = Path(episode).parts[-1]
-
-            # format episode number
-            ep_number = format_number(str(episode_num))
-
-            # get the episode's path
-            path = f"{os.sep}".join(list(Path(episode).parts)[1:-2])
-
-            # get remove everything before the first dash
-            new_name = f"{os.sep}{path}{os.sep}{season}{os.sep}S{season_num}E{ep_number} {episode_name[episode_name.find('-'):]}"
-
-            if "Linux" in platform.system():
-                # get the episode number
-                os.system(f'sudo mv "{episode}" "{new_name}"')
-            elif "Windows" in platform.system():
-                os.system(f'mv "{episode}" "{new_name}"')
-
-            # increment our counters
-            episode_num += 1
+    failed_files = []
+    for video in file_list:
+        try:
+            os.rename(video.file_path, video.directory / video.new_name)
             counter += 1
+        except Exception as e:
+            failed_files.append(video.file_name)
+            console.print(f"[red]Failed to rename {video.file_name} - {e}")
     # return counter for console output
+    if failed_files:
+        console.print(
+            f"[red]Failed to rename {len(failed_files)} files: {failed_files}"
+        )
     return counter
